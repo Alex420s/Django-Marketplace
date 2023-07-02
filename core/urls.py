1d2bdc17a02cba35f51d827f1c22faf83dc43816
Alex420s <edgaring1998@gmail.com>
2023-07-02 00:57:45 -0600

    ed

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 # name is for dynamic urls
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from base.views import index, contact
@@ -7,3 +8,5 @@ urlpatterns = [
     path('contact', contact, name='contact'),
     path('', index , name='index'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
