24e4127c6f74362f409061e3631b5b15f87a3643
Alex420s <edgaring1998@gmail.com>
2024-03-15 12:49:08 -0600

    hot fix

--- core/settings.py
+++ core/settings.py
@@ -134,9 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
