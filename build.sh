6d0416361d05a8906674087a83137538974668ba
Alex420s <edgaring1998@gmail.com>
2024-03-15 12:34:54 -0600

    css fix

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
 python manage.py migrate --noinput
 python manage.py makemigrations --noinput
+npm install
+npm run start
