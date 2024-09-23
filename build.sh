1ee43e9baea67df01c2ae3e7097f5cd98b81f57d
Alex Django <edgaring1998@gmail.com>
2024-09-23 15:10:59 -0600

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-# Exit on error
-set -o errexit
-
-
-python manage.py collectstatic --noinput 
-python manage.py migrate --noinput
-python manage.py makemigrations --noinput
+npm install
+npm run build
+python manage.py collectstatic --noinput
+python manage.py migrate
