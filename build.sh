5dc2c5a823a0de1ae817ec45921ca08442cc8b8f
Alex Django <edgaring1998@gmail.com>
2024-09-23 18:53:05 -0600

    Render deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+# Exit on error
+set -o errexit
 
-python manage.py collectstatic --noinput
-python manage.py migrate
+pip install -r requirements.txt
+npm install 
+npm run build
+python manage.py collectstatic --noinput --clear
+python manage.py migrate --noinput
+python manage.py makemigrations --noinput
